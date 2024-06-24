6dcd26741ef22af3d549f95d567577d96c580a10
yuniruyuni <yuniruyuni@gmail.com>
2024-06-24 17:11:09 +0900

    feat: migrate from MrDamian main repository

---
+++ index.ts
@@ -0,0 +1,45 @@
+import path from "node:path";
+import { Component } from "mrdamian/model/component";
+import type { ComponentConfig } from "mrdamian/model/parameters";
+import type { Fetch } from "mrdamian/model/server";
+import type { Field } from "mrdamian/model/variable";
+
+export type PanelConfig = ComponentConfig;
+export default class Panel extends Component<PanelConfig> {
+  get fetch(): Fetch {
+    return async (req: Request): Promise<Response> => {
+      const url = new URL(req.url);
+      if (url.pathname === "/") {
+        const f = Bun.file(path.join(import.meta.dir, "index.html"));
+        return new Response(f, { headers: { "Content-Type": f.type } });
+      }
+      if (url.pathname === "/index.js") {
+        console.log(`__dirname: ${__dirname}`);
+        console.log(`import.meta.dir: ${import.meta.dir}`);
+        console.log("fetching index.js");
+        const f = Bun.file(path.join(import.meta.dir, "root.js"));
+        console.log("in reality, it's root.js");
+        return new Response(f, { headers: { "Content-Type": f.type } });
+      }
+      if (url.pathname === "/index.css") {
+        const f = Bun.file(path.join(import.meta.dir, "index.css"));
+        return new Response(f, { headers: { "Content-Type": f.type } });
+      }
+      if (url.pathname === "/panels") {
+        return new Response(`[{"type":"button", "name": "first"},{"type":"toggle", "name": "secondary"}]`, {
+          status: 200,
+          headers: { "Content-Type": "text/javascript" },
+        });
+      }
+      return new Response("<p>Not found</p>", {
+        status: 404,
+        headers: { "Content-Type": "text/html" },
+      });
+    };
+  }
+
+  public async run(): Promise<Field> {
+    // TODO: implement
+    return undefined;
+  }
+}
